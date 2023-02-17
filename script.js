d487b1bbdce4751895400543bfa802df5e4cdab4
hoanggiang2912 <109599569+hoanggiang2912@users.noreply.github.com>
2023-02-17 10:45:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
